ab1c32e27c1dcb8f23e6cc3bc1587b6c7c0d96ff
hc precog <hcprecog@gmail.com>
2025-07-12 23:25:33 +0800

    去除 ts 类型警告

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,37 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
+import { dts } from 'rollup-plugin-dts';
 
-export default {
-  input: 'index.ts',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
-      sourcemap: true,
-      exports: 'named'
-    },
-    {
-      file: 'dist/index.esm.js',
-      format: 'esm',
-      sourcemap: true,
-      exports: 'named'
-    }
-  ],
-  plugins: [
-    typescript({
-      tsconfig: './tsconfig.json',
-      useTsconfigDeclarationDir: true
-    })
-  ],
-  external: []
-}; 
+export default [
+  {
+    input: 'index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named'
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'esm',
+        sourcemap: true,
+        exports: 'named'
+      }
+    ],
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: './dist/types',
+        outDir: './dist'
+      })
+    ],
+    external: []
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+    external: []
+  }
+]; 
